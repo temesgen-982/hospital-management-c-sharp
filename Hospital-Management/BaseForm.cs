3ed8458e05e44c8776228a31777c09a38a92e0e8
temesgen-982 <tedenadane@gmail.com>
2025-01-03 06:36:21 -0800

    Deleted Nurse Pages

--- Hospital-Management/BaseForm.cs
+++ Hospital-Management/BaseForm.cs
@@ -10,11 +10,18 @@ using System.Windows.Forms;
 
 namespace Hospital_Management
 {
-    public partial class NurseAppointments : Form
+    public partial class BaseForm : Form
     {
-        public NurseAppointments()
+        public BaseForm()
         {
             InitializeComponent();
+        }
+
+        private void LogoutButton_Click(object sender, EventArgs e)
+        {
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
+            this.Hide();
         }
     }
 }
